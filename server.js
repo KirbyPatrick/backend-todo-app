0486bd6609ab32c9485f98d1c103a134e41cac85
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-14 21:06:07 -0500

    mlab database connected

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
 
-mongoose.connect("mongodb://backend-todo-user:<dbpassword>@ds157136.mlab.com:57136/backend-todo")
+mongoose.connect(
+  "mongodb://backend-todo-user:Backend1@ds157136.mlab.com:57136/backend-todo",
+  {
+    useNewUrlParser: true
+  },
+  function(error) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("connected to the database");
+    }
+  }
+);
 
-app.use(cors());
+app.use(cors()); 
 app.use(bodyParser.json());
 
 app.listen(PORT, function() {
