8f715f9a161d2ed435d46885209bc3e1f811db27
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-14 21:26:25 -0500

    added GET via Express

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 const PORT = 4000;
+
+let Todo = require("./model");
 
 mongoose.connect(
   "mongodb://backend-todo-user:Backend1@ds157136.mlab.com:57136/backend-todo",
@@ -14,13 +17,25 @@ mongoose.connect(
     if (error) {
       console.log(error);
     } else {
-      console.log("connected to the database");
+      console.log("Connected to the MongoDB database!");
     }
   }
 );
 
-app.use(cors()); 
+app.use(cors());
 app.use(bodyParser.json());
+
+todoRoutes.route("/").get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
