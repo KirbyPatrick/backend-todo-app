faf6b12e2d0f294861f040cf598c1b3a0e65ad39
KirbyPatrick <pkirbyinatx@gmail.com>
2019-05-15 18:43:12 -0500

    added get by id and post to express

--- server.js
+++ server.js
@@ -35,6 +35,25 @@ todoRoutes.route("/").get(function(req, res) {
   });
 });
 
+todoRoutes.route("/:id").get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route("/add").post(function(req, res) {
+  let todo = new Todo(res.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new todo failed");
+    });
+});
+
 app.use("/todos", todoRoutes);
 
 app.listen(PORT, function() {
